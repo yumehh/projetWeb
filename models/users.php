bf24111ca4ff772554274536c7a189fd787a371d
yumehh <myhopeandreason@hotmail.com>
2021-02-09 14:35:55 +0100

    add modif

--- models/users.php
+++ models/users.php
@@ -1,11 +1,12 @@
 <?php 
-    require_once 'connexion_db.php';
+    require 'connexion_db.php';
 
     function addUser($pseudo, $mdp, $email){
 
         $reponse = getDB()->prepare('INSERT INTO utilisateurs(pseudo, pwUser, email, date_inscription) VALUES (:pseudo, :mdp, :email, NOW())');
         if($mdp){
             $mdp = password_hash($mdp, PASSWORD_DEFAULT);
+            echo "ajout OK de user";
         }else{
             echo "fail mot de passe";
         }
@@ -18,12 +19,12 @@
         $reponse->closeCursor();
     }
 
-    // function checkUserByPseudo($pseudo){
-    //     $reponse = getDB()->prepare('SELECT * FROM utilisateurs WHERE pseudo = :pseudo');
-    //     $reponse->execute(['pseudo' => $pseudo]);
-    //     $user = $reponse->fetch();
-    //     $reponse->closeCursor();
-    //     return $user;
-    // }
+    function checkUserByPseudo($pseudo){
+        $reponse = getDB()->prepare('SELECT * FROM utilisateurs WHERE pseudo = :pseudo');
+        $reponse->execute(['pseudo' => $pseudo]);
+        $user = $reponse->fetch();
+        $reponse->closeCursor();
+        return $user;
+    }
 
 ?>
