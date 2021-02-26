d28e69f2c7538f8e502bbc4897ad3d51202a7d02
yumehh <myhopeandreason@hotmail.com>
2021-02-26 10:50:16 +0100

    inscription fonctionne

--- controllers/inscription.php
+++ controllers/inscription.php
@@ -1,14 +1,16 @@
 <?php 
 
-    // require_once('models/users.php');
-    // require_once('views/inscription.php');
+    require_once('models/users.php');
+    require_once('views/inscription.php');
 
-    require 'models/users.php';
-    include 'views/inscription.php';
+    // require 'models/users.php';
+    // include 'views/inscription.php';
 
     if(isset($_POST['pseudo']) && $_POST['pseudo'] && $_POST['email']){
 
-        addUser($_POST['pseudo'], $_POST['mdp'], $_POST['email']);
+        $user = new Users();
+        $user->addUser($_POST['pseudo'], $_POST['mdp'], $_POST['email']);
+        return $user;
 
         header('Location: welcome');
     }
