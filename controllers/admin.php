eccadf9b8cf726531adc1abfb88d8910ee5eca6c
yumehh <myhopeandreason@hotmail.com>
2021-03-05 13:28:34 +0100

    afficher artiste admin

--- controllers/admin.php
+++ controllers/admin.php
@@ -7,34 +7,28 @@
 
     $admin = new Admin();
     $genre = new GenreMusique();
+    $afficher = new Artistes();
     $genreMusiques = $genre->getAll();
+    $afficherArtistes = $afficher->getAll();
 
     switch(REQ_TYPE_ID){
 
         case "ajoutArtistes":
-            
             require_once('views/ajoutArtistes.php');
-
             if(!empty($_POST)){
-
                 if($_POST['genreArtiste'] == "1"){
-
                     require_once('views/ajoutGenre.php');
                 }
-
                 elseif(!empty($_POST['nomArtiste']) && !empty($_POST['imageArtiste']) && !empty($_POST['descriptionArtiste']) && !empty($_POST['genreArtiste'])){
-
                    $ajout = $admin->addArtistes($_POST['nomArtiste'], $_POST['imageArtiste'], $_POST['descriptionArtiste'], $_POST['genreArtiste']);
-
                 }  
-                
-            }else{
-                
             }
             break;
 
-        case "listeArtistes":
-            require_once('views/listeArtistes.php');
+        case "afficherArtistes":
+            if($afficherArtistes){
+                require_once('views/afficherArtistes.php');
+            }
             break;    
     }
 ?>
---
+++ views/afficherArtistes.php
@@ -0,0 +1,44 @@
+<?php 
+
+    $title = "Artistes";
+    $content = "Ajouts de l'artiste";
+    //include 'includes/template.php';
+
+?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+    <table>
+        <caption>Liste des artistes </caption>
+            <thead>
+                <tr>
+                    <th> Artistes </th>
+                    <th> Genre </th>
+                </tr>
+		    </thead>
+                
+            <?php foreach($afficherArtistes as $artiste): ?>
+                <tr>
+                    <td><a href= "<?= REQ_TYPE?>/<?=$artiste['nomArtiste']?>"> <?=$artiste['nomArtiste']?> </a></td>
+                    <td><?= $artiste['nomGenre'] ?></td>
+                </tr>
+                    
+            <?php endforeach ?>   
+
+    </table>  
+        <script src="" async defer></script>
+    </body>
+</html>
