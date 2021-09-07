8053605221bf57acc744373e38e71c21e48cb328
yumehh <myhopeandreason@hotmail.com>
2021-09-07 22:58:49 +0200

    fix bootstrap

--- views/includes/template.php
+++ views/includes/template.php
@@ -6,7 +6,7 @@
         <title><?php echo $title ?></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="text/javascript" href="../../bootstrap-5.1.0-dist/js/bootstrap.bundle.min.js">
+        <script src="../../bootstrap-5.1.0-dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="../../bootstrap-5.1.0-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="../../css/style.css">
 
