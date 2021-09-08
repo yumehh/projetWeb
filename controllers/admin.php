03c132aa4455bcd70dad8ab882f58cce7d79279b
yumehh <myhopeandreason@hotmail.com>
2021-09-08 10:29:39 +0200

    modification sécurité admin/client url

--- controllers/admin.php
+++ controllers/admin.php
@@ -22,154 +22,160 @@
     $afficherMusicDeleted = $music->getMusicIsDeleted();
     $musiqueArtiste = $music->MusicByArtistId(REQ_ACTION_2);
     
-    switch(REQ_TYPE_ID){
+    if($_SESSION['userID'] == null){
+        header('location: login');
+    }else{
+        switch(REQ_TYPE_ID){
 
-        case "profil": 
-            if(REQ_ACTION){
-                $user = $client->getUserByNom(REQ_ACTION);
-                $userDetail = $client->getUserById($user['idUtilisateur']);
-                if(isset($user) && isset($userDetail)){
-                    require_once('views/profil.php');
+            case "profil": 
+                if(REQ_ACTION){
+                    $user = $client->getUserByNom(REQ_ACTION);
+                    $userDetail = $client->getUserById($user['idUtilisateur']);
+                    if(isset($user) && isset($userDetail)){
+                        require_once('views/profil.php');
+                    }
+                    if(!empty($_POST)){
+                        $modify = $client->updateUser($_POST['pseudo'], $_POST['pwUser'], $_POST['email'], $user['idUtilisateur']);
+                    }
                 }
+                break;
+    
+            case "ajoutArtistes":
+                require_once('views/ajoutArtistes.php');
                 if(!empty($_POST)){
-                    $modify = $client->updateUser($_POST['pseudo'], $_POST['pwUser'], $_POST['email'], $user['idUtilisateur']);
+                    if($_POST['genreArtiste'] == "1"){
+                        require_once('views/ajoutGenre.php');
+                    }
+                    elseif(!empty($_POST['nomArtiste']) && !empty($_FILES['imageArtiste']) && !empty($_POST['descriptionArtiste']) && !empty($_POST['genreArtiste'])){
+                        $receiveImg = insertImg($_FILES['imageArtiste']);
+                        $searchSpace = " ";
+                        $replaceSpace ="";
+                        $delSpace = str_replace($searchSpace, $replaceSpace, $_POST['nomArtiste']);
+                        $ajout = $admin->addArtistes($delSpace, $receiveImg[1], $_POST['descriptionArtiste'], $_POST['genreArtiste']);
+                    } 
                 }
-            }
-            break;
-
-        case "ajoutArtistes":
-            require_once('views/ajoutArtistes.php');
-            if(!empty($_POST)){
-                if($_POST['genreArtiste'] == "1"){
-                    require_once('views/ajoutGenre.php');
-                }
-                elseif(!empty($_POST['nomArtiste']) && !empty($_FILES['imageArtiste']) && !empty($_POST['descriptionArtiste']) && !empty($_POST['genreArtiste'])){
-                    $receiveImg = insertImg($_FILES['imageArtiste']);
-                    $searchSpace = " ";
-                    $replaceSpace ="";
-                    $delSpace = str_replace($searchSpace, $replaceSpace, $_POST['nomArtiste']);
-                    $ajout = $admin->addArtistes($delSpace, $receiveImg[1], $_POST['descriptionArtiste'], $_POST['genreArtiste']);
-                } 
-            }
-            break;
-
-        case "afficherArtistes":
-            if(REQ_ACTION){
-                $detailArtiste = $afficher->getByNom(REQ_ACTION);
-                $artistMusics = $afficher->getInfoArtiste($detailArtiste['idArtiste']);     
-                    if(isset($detailArtiste) && isset($artistMusics)){
-                        if(!empty($_SESSION)){
-                            if(!empty($_POST['titreMusique']) && !empty($_POST['prixMusique'])){
-                            $addMusics = $music->addMusic($_POST['titreMusique'], $_POST['prixMusique'], $detailArtiste['idArtiste']);
+                break;
+    
+            case "afficherArtistes":
+                if(REQ_ACTION){
+                    $detailArtiste = $afficher->getByNom(REQ_ACTION);
+                    $artistMusics = $afficher->getInfoArtiste($detailArtiste['idArtiste']);     
+                        if(isset($detailArtiste) && isset($artistMusics)){
+                            if(!empty($_SESSION)){
+                                if(!empty($_POST['titreMusique']) && !empty($_POST['prixMusique'])){
+                                $addMusics = $music->addMusic($_POST['titreMusique'], $_POST['prixMusique'], $detailArtiste['idArtiste']);
+                                }
+                            require_once('views/detailArtiste.php');
                             }
-                        require_once('views/detailArtiste.php');
+                        }else{
+                            require_once('views/404.php');
                         }
-                    }else{
-                        require_once('views/404.php');
+                }else{
+                        $artistes = $afficher->getAll();
+                        if($artistes){
+                            require_once('views/afficherArtistes.php');
+                        }else{
+                            require_once('views/404.php');
+                        }
                     }
-            }else{
+                break;
+            
+            case "modifierArtistes":
                     $artistes = $afficher->getAll();
-                    if($artistes){
-                        require_once('views/afficherArtistes.php');
-                    }else{
-                        require_once('views/404.php');
+                    require_once('views/modifierArtistes.php');
+                    $detailArtiste = $afficher->getByNom(REQ_ACTION_2);
+                    if(!empty($_POST)){
+                        $receiveImg = insertImg($_FILES['imageArtiste']);
+                        $modify = $admin->updateArtistes($_POST['nomArtiste'], $receiveImg[1], $_POST['descriptionArtiste'], $_POST['genreArtiste'], $detailArtiste['idArtiste']);
                     }
-                }
-            break;
-        
-        case "modifierArtistes":
+                break;      
+            
+            case "supprimerArtistes":
                 $artistes = $afficher->getAll();
-                require_once('views/modifierArtistes.php');
-                $detailArtiste = $afficher->getByNom(REQ_ACTION_2);
+                require_once('views/supprimerArtistes.php');
                 if(!empty($_POST)){
-                    $receiveImg = insertImg($_FILES['imageArtiste']);
-                    $modify = $admin->updateArtistes($_POST['nomArtiste'], $receiveImg[1], $_POST['descriptionArtiste'], $_POST['genreArtiste'], $detailArtiste['idArtiste']);
+                    $delete = $admin->deleteArtistes(REQ_ACTION);
                 }
-            break;      
-        
-        case "supprimerArtistes":
-            $artistes = $afficher->getAll();
-            require_once('views/supprimerArtistes.php');
-            if(!empty($_POST)){
-                $delete = $admin->deleteArtistes(REQ_ACTION);
-            }
-           break;
-
-        case "restaurerArtistes":
-           $artistesDeleted = $admin->getArtistIsDeleted();
-            require_once('views/restaurerArtistes.php');
-            foreach($artistesDeleted as $deleted){
-                $idDeleted = $deleted['idArtiste'];
-                $restore = $admin->restoreArtiste($idDeleted);
-            }
-            break;
-        
-        case "afficherMusiques":
-            $musique = $music->getAll();
-            if($musique){
-                require_once('views/afficherMusiques.php');
-            }else{
-                require_once('views/404.php');
-            }
-            break;
-        
-        case "detailArtiste":
-
-            $detailArtiste = $afficher->getByNom(REQ_ACTION);
-                if(isset($getMusicArtist)){
-                    require_once('views/detailArtiste.php');
+               break;
+    
+            case "restaurerArtistes":
+               $artistesDeleted = $admin->getArtistIsDeleted();
+                require_once('views/restaurerArtistes.php');
+                foreach($artistesDeleted as $deleted){
+                    $idDeleted = $deleted['idArtiste'];
+                    $restore = $admin->restoreArtiste($idDeleted);
+                }
+                break;
+            
+            case "afficherMusiques":
+                $musique = $music->getAll();
+                if($musique){
+                    require_once('views/afficherMusiques.php');
                 }else{
                     require_once('views/404.php');
                 }
                 break;
-
-        case "modifierMusics":
+            
+            case "detailArtiste":
+    
+                $detailArtiste = $afficher->getByNom(REQ_ACTION);
+                    if(isset($getMusicArtist)){
+                        require_once('views/detailArtiste.php');
+                    }else{
+                        require_once('views/404.php');
+                    }
+                    break;
+    
+            case "modifierMusics":
+                    $musique = $music->getAll();
+                    require_once('views/modifierMusics.php');
+                    $musicById = $music->getIdMusic(REQ_ACTION);
+                    if(!empty($_POST)){
+                        $modify = $music->updateMusic($_POST['nomMusique'], $_POST['prixMusique'], $_POST['idArtiste'], $musicById['idMusique']);
+                    }
+                break; 
+                
+            case "supprimerMusics":
                 $musique = $music->getAll();
-                require_once('views/modifierMusics.php');
-                $musicById = $music->getIdMusic(REQ_ACTION);
+                require_once('views/supprimerMusics.php');
                 if(!empty($_POST)){
-                    $modify = $music->updateMusic($_POST['nomMusique'], $_POST['prixMusique'], $_POST['idArtiste'], $musicById['idMusique']);
+                    $delete = $music->deleteMusic(REQ_ACTION);
                 }
-            break; 
-            
-        case "supprimerMusics":
-            $musique = $music->getAll();
-            require_once('views/supprimerMusics.php');
-            if(!empty($_POST)){
-                $delete = $music->deleteMusic(REQ_ACTION);
-            }
-            break;
-
-        case "restaurerMusiques":
-           $musiqueDeleted = $music->getMusicIsDeleted();
-           foreach($musiqueDeleted as $deleted){
-                $idDeleted = $deleted['idMusique'];
-                $restore = $music->restoreMusics($idDeleted);
-            }
-            require_once('views/restaurerMusiques.php');
-            break;
-            
-        case "listingCommande":
-            if(isset($_SESSION['userID'])){
-                foreach($_SESSION['userID'] as $userID){
-                    $userID;
+                break;
+    
+            case "restaurerMusiques":
+               $musiqueDeleted = $music->getMusicIsDeleted();
+               foreach($musiqueDeleted as $deleted){
+                    $idDeleted = $deleted['idMusique'];
+                    $restore = $music->restoreMusics($idDeleted);
                 }
-                $getCmd = $panier->getAllCommand();
-                if(!empty($_POST['annuleCmd'])){
-                    $annuleCmd = $admin->annuleCommande($_POST['annuleCmd']);
-                }elseif(!empty($_POST['validCmd'])){
-                    $validCmd = $admin->validCommande($_POST['validCmd']);
+                require_once('views/restaurerMusiques.php');
+                break;
+                
+            case "listingCommande":
+                if(isset($_SESSION['userID'])){
+                    foreach($_SESSION['userID'] as $userID){
+                        $userID;
+                    }
+                    $getCmd = $panier->getAllCommand();
+                    if(!empty($_POST['annuleCmd'])){
+                        $annuleCmd = $admin->annuleCommande($_POST['annuleCmd']);
+                    }elseif(!empty($_POST['validCmd'])){
+                        $validCmd = $admin->validCommande($_POST['validCmd']);
+                    }
+                    require_once('views/listingCommande.php');
+                }else{
+                    require_once('views/listingCommande.php');
                 }
-                require_once('views/listingCommande.php');
-            }else{
-                require_once('views/listingCommande.php');
-            }
-            break;
-
-        case "statsCommande":
-            $getUser = $admin->getUser();
-            $test = json_encode($getUser);
-            require_once('views/statsCommande.php');
-            break;
+                break;
+    
+            case "statsCommande":
+                $getUser = $admin->getUser();
+                $test = json_encode($getUser);
+                require_once('views/statsCommande.php');
+                break;
+        }
     }
+
+    
 ?>
--- controllers/clients.php
+++ controllers/clients.php
@@ -17,80 +17,85 @@
     $afficherArtistes = $afficher->getAll();
     $afficherMusic = $music->getAll();
 
-    switch(REQ_TYPE_ID){
+    if($_SESSION['userID'] == null){
+        header('location: login');
+    }else{
+        switch(REQ_TYPE_ID){
     
-        case "profil":
-            if(REQ_ACTION){
-                $user = $client->getUserByNom(REQ_ACTION);
-                $userDetail = $client->getUserById($user['idUtilisateur']);
-                if(isset($user) && isset($userDetail)){
-                    require_once('views/profil.php');
+            case "profil":
+                if(REQ_ACTION){
+                    $user = $client->getUserByNom(REQ_ACTION);
+                    $userDetail = $client->getUserById($user['idUtilisateur']);
+                    if(isset($user) && isset($userDetail)){
+                        require_once('views/profil.php');
+                    }
+                    if(!empty($_POST)){
+                        $modify = $client->updateUser($_POST['pseudo'], $_POST['pwUser'], $_POST['email'], $user['idUtilisateur']);
+                    }
                 }
+                break;
+    
+            case "artistes":
+                if(REQ_ACTION){
+                    $detailArtiste = $afficher->getByNom(REQ_ACTION);
+                    $musiqueArtiste = $music->MusicByArtistId($detailArtiste['idArtiste']);
+                    if(isset($musiqueArtiste)){
+                        require_once('views/detailArtisteClient.php');
+                    }
+                }else{
+                    require_once('views/artistes.php');
+                }
+                break;
+    
+            case "panier":
+                $getMusic = null;
+                $prix = null;
                 if(!empty($_POST)){
-                    $modify = $client->updateUser($_POST['pseudo'], $_POST['pwUser'], $_POST['email'], $user['idUtilisateur']);
-                }
-            }
-            break;
-
-        case "artistes":
-            if(REQ_ACTION){
-                $detailArtiste = $afficher->getByNom(REQ_ACTION);
-                $musiqueArtiste = $music->MusicByArtistId($detailArtiste['idArtiste']);
-                if(isset($musiqueArtiste)){
-                    require_once('views/detailArtisteClient.php');
-                }
-            }else{
-                require_once('views/artistes.php');
-            }
-            break;
-
-        case "panier":
-            $getMusic = null;
-            $prix = null;
-            if(!empty($_POST)){
-                //affichage
-                $getMusic = $music->getMusicById($_POST['achatMusique']);
-                //compta panier
-                $idMusicPanier = $music->getIdMusic($_POST['achatMusique']);
-                $musicPrixPanier = $music->getPrixByIdPanier($_POST['achatMusique']);
-                if(!empty($idMusicPanier || !empty($musicPrixPanier))){
-                    foreach($_SESSION['userID'] as $userID){
-                        $userID;
+                    //affichage
+                    $getMusic = $music->getMusicById($_POST['achatMusique']);
+                    //compta panier
+                    $idMusicPanier = $music->getIdMusic($_POST['achatMusique']);
+                    $musicPrixPanier = $music->getPrixByIdPanier($_POST['achatMusique']);
+                    if(!empty($idMusicPanier || !empty($musicPrixPanier))){
+                        foreach($_SESSION['userID'] as $userID){
+                            $userID;
+                        }
+                        foreach($musicPrixPanier as $prixMusic){
+                            $prixMusic[0];
+                        }
+                        //je stocke dans un tableau l'idMusique
+                        $tempId = array();
+                        $tempId['idMusique'] = $idMusicPanier;
+                        $tempId['qte'] = 1;
+                        $tempId['prixMusique'] = $prixMusic['prix'];
+                        $panier->creationPanier();
+                        if(!empty($_SESSION['panier'])){
+                            $panier->ajoutPanier($tempId);
+                            $prix = $panier->montant_panier();   
+                            $panier->addCommande($userID, $_POST['achatMusique']);
+                        }
+                            require_once('views/panier.php');
+                    }else{
+                            require_once('views/panier.php');
                     }
-                    foreach($musicPrixPanier as $prixMusic){
-                        $prixMusic[0];
-                    }
-                    //je stocke dans un tableau l'idMusique
-                    $tempId = array();
-                    $tempId['idMusique'] = $idMusicPanier;
-                    $tempId['qte'] = 1;
-                    $tempId['prixMusique'] = $prixMusic['prix'];
-                    $panier->creationPanier();
-                    if(!empty($_SESSION['panier'])){
-                        $panier->ajoutPanier($tempId);
-                        $prix = $panier->montant_panier();   
-                        $panier->addCommande($userID, $_POST['achatMusique']);
-                    }
-                        require_once('views/panier.php');
                 }else{
                         require_once('views/panier.php');
+                    }  
+                    break;
+    
+            case "achats":
+                foreach($_SESSION['userID'] as $userID){
+                    $userID;
                 }
-            }else{
-                    require_once('views/panier.php');
-                }  
+                $getCmd = $panier->getAllCommandByID($userID);
+                require_once('views/achats.php');
+                if(!empty($_POST['annuleCmd'])){
+                    $annuleCmd = $panier->annuleCommande($_POST['annuleCmd'], $userID);
+                }elseif(!empty($_POST['validCmd'])){
+                    $validCmd = $panier->validCommande($_POST['validCmd'], $userID);
+                }
                 break;
-
-        case "achats":
-            foreach($_SESSION['userID'] as $userID){
-                $userID;
-            }
-            $getCmd = $panier->getAllCommandByID($userID);
-            require_once('views/achats.php');
-            if(!empty($_POST['annuleCmd'])){
-                $annuleCmd = $panier->annuleCommande($_POST['annuleCmd'], $userID);
-            }elseif(!empty($_POST['validCmd'])){
-                $validCmd = $panier->validCommande($_POST['validCmd'], $userID);
-            }
-            break;
+        }
     }
+
 ?>
